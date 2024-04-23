b46246ae07d3a0271cdfcf4268522e89d1a57f9a
SYAN <syanays.git@gmail.com>
2024-04-23 14:35:47 +0300

    duplicate item

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { TodoObject, DOM, TODO } from "./todo";
 import './style.css';
 
-const todo1 = TodoObject('Fitness', 'Play volleyball', new Date().toLocaleDateString(), 'high');
+const todo1 = TodoObject('Fitness', 'Play volleyball', new Date().toLocaleDateString(), 3);
 
+DOM.addTodoItem(todo1);
 DOM.addTodoItem(todo1);
