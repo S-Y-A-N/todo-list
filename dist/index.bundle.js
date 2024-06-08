dcff1859c732a62fa433c4d36d24fc9985208625
SYAN <syanays.git@gmail.com>
2024-06-08 22:20:59 +0300

    Rebuild

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -123,8 +123,8 @@ const DOM = (function () {
 Project.create('inbox');
 
 let t1 = TODO.create('Apply for job', "", Project.get(0), 0, new Date().toLocaleDateString(), false);
-console.log(Project.get(0).title)
-DOM.addTodoItem(t1);
+
+// DOM.addTodoItem(t1);
 
 /***/ })
 
