8ce1d83d4f785da4e1c76597807f164c844ce6dd
SYAN <syanays.git@gmail.com>
2024-06-10 04:20:46 +0300

    add auto_grow function

--- src/index.js
+++ src/index.js
@@ -8,3 +8,13 @@ Project.create('inbox');
 let t1 = TODO.create('Apply for job', "", Project.get(0), 0, new Date().toLocaleDateString(), false);
 console.log(Project.get(0).title)
 DOM.addTodoItem(t1);
+
+
+// visaul
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px";
+}
+
+document.getElementById('name').addEventListener('input', e => auto_grow(e.target))
+document.getElementById('desc').addEventListener('input', e => auto_grow(e.target))
