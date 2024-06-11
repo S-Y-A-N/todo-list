27c8d3a6be6070b0c5c960866b920400ad44e4ef
SYAN <syanays.git@gmail.com>
2024-06-11 04:07:21 +0300

    write change_theme function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './dialog.css'
 
 Project.create('inbox');
 
-let t1 = TODO.create('Apply for job', "", Project.get(0), 0, new Date().toLocaleDateString(), false);
+let t1 = TODO.create('Work on this shitty todo app :(', "", Project.get(0), 0, new Date().toLocaleDateString(), false);
 console.log(Project.get(0).title)
 DOM.addTodoItem(t1);
 
@@ -16,5 +16,13 @@ function auto_grow(element) {
     element.style.height = (element.scrollHeight) + "px";
 }
 
+function change_theme() {
+    document.documentElement.classList.toggle('dark');
+    document.getElementById('lightModeIcon').classList.toggle('hidden');
+    document.getElementById('darkModeIcon').classList.toggle('hidden');
+}
+
 document.getElementById('name').addEventListener('input', e => auto_grow(e.target))
 document.getElementById('desc').addEventListener('input', e => auto_grow(e.target))
+
+document.getElementById('themeBtn').addEventListener('click', change_theme)
