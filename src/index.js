08c5e27eaf0ccdf2e37bf896884080947fb894ef
SYAN <syanays.git@gmail.com>
2024-06-15 18:19:06 +0300

    add: check_circle_done, task_hover

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import { DOM, TODO, Project } from "./todo";
+import { TODO, Project } from "./todo";
+import { DOM } from "./dom";
+
 import './style.css';
 import './sidebar.css';
 import './dialog.css'
 
-Project.create('inbox');
+Project.create('Inbox');
+Project.create('Todo app');
+DOM.addProject(Project.get(0));
+DOM.addProject(Project.get(1));
 
 let t1 = TODO.create('Work on this shitty todo app :(', "", Project.get(0), 0, new Date().toLocaleDateString(), false);
-console.log(Project.get(0).title)
 DOM.addTodoItem(t1);
 
 
-// visaul
+
+
+
+
+
+
+// Visual DOM stuff
 function auto_grow(element) {
     element.style.height = "5px";
     element.style.height = (element.scrollHeight) + "px";
@@ -37,16 +47,35 @@ function change_page(e) {
     clickedTab.classList.add('selected')
 }
 
+function check_circle_done(e) {
+    e.target.classList.toggle('check-circle-done')
+}
+
+function task_hover(e) {
+    const editBtn = this.getElementsByClassName('edit-task-button')[0];
+    if(e.type === "mouseenter") editBtn.classList.add('visible')
+    else editBtn.classList.remove('visible')
+}
+
+// event listeners
 document.getElementById('name').addEventListener('input', e => auto_grow(e.target))
 document.getElementById('desc').addEventListener('input', e => auto_grow(e.target))
 
 document.getElementById('themeBtn').addEventListener('click', change_theme)
 
+document.getElementsByClassName('check-circle')[0].addEventListener('click', check_circle_done);
+
+// on task hover
+const tasks = document.getElementsByClassName('task');
+
+Array.from(tasks).forEach(task => {
+    task.addEventListener('mouseenter', task_hover);
+    task.addEventListener('mouseleave', task_hover);
+})
 
 // page changing via sidebar tabs
 const pageTabs = document.getElementById('pageTabs').getElementsByTagName('button');
 
 Array.from(pageTabs).forEach(button => {
-    
     button.addEventListener('click', change_page)
 });
