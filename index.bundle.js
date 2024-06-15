24377ef14a737a66d8ee01fa4381d2b4085591f9
SYAN <syanays.git@gmail.com>
2024-06-15 18:23:17 +0300

    Rebuild

--- index.bundle.js
+++ index.bundle.js
@@ -1,7 +1,7 @@
 "use strict";
 (self["webpackChunktodo_list"] = self["webpackChunktodo_list"] || []).push([[57],{
 
-/***/ 407:
+/***/ 658:
 /***/ (() => {
 
 
@@ -38,6 +38,8 @@ const Project = (function () {
 
     return { create, get }
 })();
+;// CONCATENATED MODULE: ./src/dom.js
+
 
 const DOM = (function () {
     const taskList = document.getElementById('taskList');
@@ -46,27 +48,54 @@ const DOM = (function () {
     const addTodoItem = (todo) => {
         handleNoTasks();
         
+        // task container
         const taskDiv = document.createElement('div');
+        taskDiv.classList.add('task');
 
+        // task container levels
+        const taskLv1 = document.createElement('div');
+        const taskLv2 = document.createElement('div');
+        const taskLv3 = document.createElement('div');
+
+        // task info
         const taskProject = document.createElement('span');
-        const taskDesc = document.createElement('span');
+        const taskTitle = document.createElement('span');
         const taskDate = document.createElement('span');
 
         taskProject.textContent = todo.project.name;
-        taskDesc.textContent = todo.name;
+        taskTitle.textContent = todo.name;
         taskDate.textContent = todo.dueDate;
 
-        taskDiv.classList.add('task');
-
         taskProject.classList.add('task-project');
-        taskDesc.classList.add('task-desc');
+        taskTitle.classList.add('task-title');
         taskDate.classList.add('task-date');
 
-        taskDiv.appendChild(taskProject);
-        taskDiv.appendChild(taskDesc);
-        taskDiv.appendChild(taskDate);
+        // extra elements
+        const check_circle = document.createElement('div');
+        check_circle.classList.add('check-circle');
 
+        const more_vert = document.createElement('img');
+        more_vert.setAttribute('src', './resources/more_vert_24dp_FILL0_wght400_GRAD0_opsz24.svg');
+        more_vert.classList.add('edit-task-button')
+
+        const line = document.createElement('div');
+        line.classList.add('line');
+
+        // appending to task levels
+        taskLv1.appendChild(check_circle);
+        taskLv1.appendChild(taskTitle);
+        taskLv3.appendChild(taskProject);
+        // taskLv1.appendChild(taskDate);
+        taskLv1.appendChild(more_vert);
+
+        // appending levels to task container
+        taskDiv.appendChild(taskLv1)
+        taskDiv.appendChild(taskLv2)
+        taskDiv.appendChild(taskLv3)
+
+        // appending task container to task list
         taskList.appendChild(taskDiv);
+        taskList.appendChild(line);
     }
     const isTaskListEmpty = () => TODO.isTaskListEmpty();
 
@@ -101,17 +130,26 @@ const DOM = (function () {
         addDialog.close();
     }
 
+    const addProject = (project) => {
+        addProjectToDialog(project)
+    }
 
-    const dialogBtn = document.getElementById('dialogBtn');
-    const closeDialogBtn = document.getElementById('closeDialogBtn');
-    const submitTask = document.getElementById('submitTask')
+    const addProjectToDialog = (project) => {
+        const select = document.getElementById('project');
+        const option = document.createElement('option');
+        option.textContent = project.name;
+        select.appendChild(option);
+    }
 
-    dialogBtn.addEventListener('click', () => addDialog.showModal());
-    closeDialogBtn.addEventListener('click', closeDialog);
-    submitTask.addEventListener('click', extractTodoInfo);
+    const addProjectToSidebar = () => {
+        const projectDiv = document.createElement('div')
+    }
 
+    document.getElementById('dialogBtn').addEventListener('click', () => addDialog.showModal());
+    document.getElementById('closeDialogBtn').addEventListener('click', closeDialog);
+    document.getElementById('submitTask').addEventListener('click', extractTodoInfo);
 
-    return { addTodoItem }
+    return { addTodoItem, addProject }
 
 })();
 ;// CONCATENATED MODULE: ./src/index.js
@@ -120,14 +158,24 @@ const DOM = (function () {
 
 
 
-Project.create('inbox');
+
+
+Project.create('Inbox');
+Project.create('Todo app');
+DOM.addProject(Project.get(0));
+DOM.addProject(Project.get(1));
 
 let t1 = TODO.create('Work on this shitty todo app :(', "", Project.get(0), 0, new Date().toLocaleDateString(), false);
-console.log(Project.get(0).title)
 DOM.addTodoItem(t1);
 
 
-// visaul
+
+
+
+
+
+
+// Visual DOM stuff
 function auto_grow(element) {
     element.style.height = "5px";
     element.style.height = (element.scrollHeight) + "px";
@@ -154,17 +202,36 @@ function change_page(e) {
     clickedTab.classList.add('selected')
 }
 
+function check_circle_done(e) {
+    e.target.classList.toggle('check-circle-done')
+}
+
+function task_hover(e) {
+    const editBtn = this.getElementsByClassName('edit-task-button')[0];
+    if(e.type === "mouseenter") editBtn.classList.add('visible')
+    else editBtn.classList.remove('visible')
+}
+
+// event listeners
 document.getElementById('name').addEventListener('input', e => auto_grow(e.target))
 document.getElementById('desc').addEventListener('input', e => auto_grow(e.target))
 
 document.getElementById('themeBtn').addEventListener('click', change_theme)
 
+document.getElementsByClassName('check-circle')[0].addEventListener('click', check_circle_done);
+
+// on task hover
+const tasks = document.getElementsByClassName('task');
+
+Array.from(tasks).forEach(task => {
+    task.addEventListener('mouseenter', task_hover);
+    task.addEventListener('mouseleave', task_hover);
+})
 
 // page changing via sidebar tabs
 const pageTabs = document.getElementById('pageTabs').getElementsByTagName('button');
 
 Array.from(pageTabs).forEach(button => {
-    
     button.addEventListener('click', change_page)
 });
 
@@ -173,6 +240,6 @@ Array.from(pageTabs).forEach(button => {
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__(407));
+/******/ var __webpack_exports__ = (__webpack_exec__(658));
 /******/ }
 ]);
