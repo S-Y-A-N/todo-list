21ba8f9e67d4f2f458873a0089546caf98d364c4
SYAN <syanays.git@gmail.com>
2024-07-04 16:56:12 +0300

    add isWithinOneWeek, fix makeNewDate

--- src/utility.js
+++ src/utility.js
@@ -6,14 +6,24 @@ export function generateUniqueId() {
 }
 
 export function isDate(date) {
+    // console.log(`isDate: ${date instanceof Date && !isNaN(date)}`)
     return date instanceof Date && !isNaN(date);
 }
 
 export function makeNewDate(dateString) {
     if(dateString) {
-        const parsedDate = parse(dateString, 'd MMM yyyy', new Date());
-        return startOfDay(parsedDate);
+        if (dateString.length === 10) {
+            console.log('calander date!')
+            const parsedDate = parse(dateString, 'yyyy-MM-dd', new Date());
+            return startOfDay(parsedDate);
+        } else {
+            console.log(dateString)
+            console.log('ugh? some date string')
+            return startOfDay(new Date(dateString));
+        }
     } else {
+        console.log(dateString)
+        console.log('no date string!')
         return startOfDay(new Date());
     }
 }
@@ -22,4 +32,11 @@ export function makeFutureDate(daysToAdd) {
     const currentDate = makeNewDate();
     const futureDate = addDays(currentDate, daysToAdd);
     return futureDate;
+}
+
+export function isWithinOneWeek(dueDate, currentDate) {
+    return isWithinInterval(dueDate, {
+        start: currentDate,
+        end: makeFutureDate(7),
+    });
 }
