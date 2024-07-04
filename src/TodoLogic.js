2d7ce0cbf9bdb06fda5ba24d7e6569d45761f512
SYAN <syanays.git@gmail.com>
2024-07-04 16:55:33 +0300

    add more methods: delete, get by id, update, toggle complete

--- src/TodoLogic.js
+++ src/TodoLogic.js
@@ -1,4 +1,4 @@
-import { generateUniqueId, isDate } from "./utility";
+import { generateUniqueId, isDate, makeNewDate } from "./utility";
 
 class TodoItem {
     constructor({
@@ -14,7 +14,7 @@ class TodoItem {
         this.desc = desc;
         this.project = project;
         this.priority = priority;
-        this.dueDate = isDate(dueDate) ? dueDate : "";
+        this.dueDate = isDate(dueDate) ? dueDate : makeNewDate(dueDate);
         this.complete = complete || false;
     }
 
@@ -40,11 +40,47 @@ export default class TodoLogic {
         return this.todoList.length === 0;
     }
 
-    createTodo(data) {
-        const newTodo = new TodoItem(data)
+    createTodo(todoData) {
+        const newTodo = new TodoItem(todoData)
         this.todoList.push(newTodo);
         this.saveToLocal();
         return newTodo;
+    }
+
+    deleteTodo(todoId) {
+        const index = this.todoList.findIndex((todo) => todo.id === todoId);
+        if (index !== -1) {
+            this.todoList.splice(index, 1);
+            this.saveToLocal();
+        }
+    }
+
+    getTodoById(todoId) {
+        const todo = this.todoList.find((todo) => todo.id === todoId)
+        if (todo) {
+            return todo;
+        }
+    }
+
+    updateTodo(todoId, todoData) {
+        const todo = this.todoList.find((todo) => todo.id === todoId);
+        if (todo) {
+            todo.name = todoData.name;
+            todo.desc = todoData.desc;
+            todo.project = todoData.project;
+            todo.priority = todoData.priority;
+            todo.dueDate = makeNewDate(todoData.dueDate);
+
+            this.saveToLocal();
+        }
+    }
+
+    todoItemToggleComplete(todoId) {
+        const todo = this.todoList.find((todo) => todo.id === todoId);
+        if (todo) {
+          todo.toggleComplete();
+          this.saveToLocal();
+        }
     }
 
     saveToLocal() {
