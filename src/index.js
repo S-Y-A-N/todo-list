5d2c6674d3ae3714ac5a827a4a75b507d915345e
SYAN <syanays.git@gmail.com>
2024-07-11 19:17:29 +0300

    minor deletions

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import TodoView from './View';
 import TodoController from './Controller';
 
 document.addEventListener('DOMContentLoaded', () => {
-    localStorage.clear();
     const todoLogic = new TodoLogic();
     const projectLogic = new ProjectLogic();
     const view = new TodoView();
@@ -18,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
-// Visual DOM stuff / may be discarded
+// Visual DOM stuff / to be discarded
 
 // function check_circle_done() {
 //     this.classList.toggle('check-circle-done');
@@ -28,21 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
 //     } else {
 //         todoTitle.style.textDecoration = 'none';
 //     }
-
-// }
-
-// function todo_hover(e) {
-//     const editBtn = this.getElementsByClassName('edit-todo-button')[0];
-//     if (e.type === "mouseenter") editBtn.classList.add('visible')
-//     else editBtn.classList.remove('visible')
 // }
 
 // document.getElementsByClassName('check-circle')[0].addEventListener('click', check_circle_done);
-
-// // on todo hover
-// const todos = document.getElementsByClassName('todo');
-// Array.from(todos).forEach(todo => {
-//     todo.addEventListener('mouseenter', todo_hover);
-//     todo.addEventListener('mouseleave', todo_hover);
-//     console.log(todos)
-// });
--- src/utility.js
+++ src/utility.js
@@ -13,17 +13,12 @@ export function isDate(date) {
 export function makeNewDate(dateString) {
     if(dateString) {
         if (dateString.length === 10) {
-            console.log('calander date!')
             const parsedDate = parse(dateString, 'yyyy-MM-dd', new Date());
             return startOfDay(parsedDate);
         } else {
-            console.log(dateString)
-            console.log('ugh? some date string')
             return startOfDay(new Date(dateString));
         }
     } else {
-        console.log(dateString)
-        console.log('no date string!')
         return startOfDay(new Date());
     }
 }
